771743144a9b2c290cba00458d7e76964dc6b5d8
pacoxrty <a3519110015@alumno.uttehuacan.edu.mx>
2022-10-05 21:35:04 -0500

    Actualizar formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -12,6 +12,7 @@
           proyecto: this.proyecto,
           tipo: this.tipo,
           urgente: this.urgente,
+          completado: false,
         };
         this.proyectos.push(proyecto);
 
@@ -21,55 +22,98 @@
         this.urgente = false;
 
       },
+      cambiarEstado (proyecto, campo){
+        //this.proyectos[id].urgente = !this.proyectos[id].urgente;
+        //console.log(proyecto, campo)
+        proyecto[campo] = !proyecto[campo];
+      }
+    },
+    computed:{
+      numeroProyectos () {
+        return this.proyectos.length;
+      },
+      porcentaje(){
+        let completados = 0;
+
+        this.proyectos.map(proyecto => {
+          if (proyecto.completado) completados++;
+        });
+        console.log((completados * 100) / this.numeroProyectos);
+        return 
+      },
     },
   };
 </script>
 <template>
-    <form @submit.prevent="registrarProyecto">
-        <div class="mb-3">
-          <label class="form-label">Proyecto</label>
-          <input v-model.trim="proyecto" type="text" class="form-control" required />
+      <div class="row">
+
+        <div class="col-12 mb-4">
+          <h3 class="text-center">Progreso 0%</h3>
+
+          <div class="progress">
+            <div class="progress-bar progress-bar-striped progress-bar-animated
+             bg-success" role="progressbar" 
+             aria-valuenow="25"
+             aria-valuemin="0"
+             aria-valuemax="100"
+             style="width: 50%"></div>
+          </div>
         </div>
 
-        <div class="mb-3">
-          <label class="form-label">Actividad</label>
-          <select v-model.trim="tipo" required class="form-select">
-            <option disabled  selected value= "">Selecciona un tipo de actividad</option>
-            <option >Aplicacion Web con Vue.js</option>
-            <option  >BackendServices con Node.js</option>
-            <option  >App móvil con React Native</option>
-          </select>
+
+
+        <div class="col-12 col-md-4">
+          <form @submit.prevent="registrarProyecto">
+            <div class="mb-3">
+              <label class="form-label">Proyecto</label>
+              <input v-model.trim="proyecto" type="text" class="form-control" required />
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">Actividad</label>
+              <select v-model.trim="tipo" required class="form-select">
+                <option disabled  selected value= "">Selecciona un tipo de actividad</option>
+                <option >Aplicacion Web con Vue.js</option>
+                <option  >BackendServices con Node.js</option>
+                <option  >App móvil con React Native</option>
+              </select>
+            </div>
+
+            <div class="mb-3">
+              <label  for="exampleInputPassword1" class="form-check-lable">Urgente</label>
+              <input v-model="urgente" type="checkbox" class="form-ccheck-input" />
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Guardar</button>
+          </form>
         </div>
+        <div class="col-12 col-md-8">
+          <h3>Total de proyectos: {{numeroProyectos}}</h3>
 
-        <div class="mb-3">
-          <label  for="exampleInputPassword1" class="form-check-lable">Urgente</label>
-          <input v-model="urgente" type="checkbox" class="form-ccheck-input" />
+          <div class="table reponsive">
+            <table class="table table-dark table-hover">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Proyecto</th>
+                  <th>Tipo</th>
+                  <th>Urgente</th>
+                  <th>Completado</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr v-for="(proyecto, index) in proyectos" :key="index">
+                  <td>{{ index + 1 }}</td>
+                  <td>{{ proyecto.proyecto }}</td>
+                  <td>{{ proyecto.tipo }}</td>
+                  <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{ proyecto.urgente ? "Si" : "No"}}</td>
+                  <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'">{{ proyecto.completado ? "Completado" : "Incompleto"}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-        
-        <button type="submit" class="btn btn-primary">Guardar</button>
-      </form>
-      <hr>
-
-      <div class="table reponsive">
-        <table class="table table-dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Proyecto</th>
-              <th>Tipo</th>
-              <th>Urgente</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Laravel app</td>
-              <td>Laravel</td>
-              <td class="bg-danger">true</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
-     
+      {{porcentaje}}
 </template>
